2f2d579fa36a902d2912bdd412b595240acb69f3
Carolinegit config --global user.name Carolinegit config --global user.name Caroline <huang244@msu.edu>
2025-10-21 22:45:52 -0400

    HI

---
+++ 4keymatrix/4keymatrix_copy_20251021224456/4keymatrix_copy_20251021224456.ino
@@ -0,0 +1,28 @@
+int rowPins[2] = {15, 14}; // R0, R1
+int colPins[2] = {10, 16}; // C0, C1
+
+bool keyState[2][2] = { {false,false}, {false,false} };
+
+void setup() {
+  for (int c = 0; c < 2; c++) pinMode(colPins[c], INPUT_PULLUP);
+  for (int r = 0; r < 2; r++) {
+    pinMode(rowPins[r], OUTPUT);
+    digitalWrite(rowPins[r], HIGH);
+  }
+}
+
+void loop() {
+  for (int r = 0; r < 2; r++) {
+    for (int i = 0; i < 2; i++) digitalWrite(rowPins[i], (i == r) ? LOW : HIGH);
+    for (int c = 0; c < 2; c++) {
+      bool pressed = (digitalRead(colPins[c]) == LOW);
+      if (pressed && !keyState[r][c]) {
+        keyState[r][c] = true;
+        Serial.print(r);
+        Serial.println(c);
+      } else if (!pressed && keyState[r][c]) {
+        keyState[r][c] = false;
+      }
+    }
+  }
+}
